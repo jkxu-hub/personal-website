0e477cdb0d7c0fd87990c6cbc0831be75dd56313
Jeffrey Xu <jkxu@buffalo.edu>
2024-08-10 10:02:33 -0400

    Initial Repo

---
+++ scripts/global.js
@@ -0,0 +1,31 @@
+const blurBackground = document.getElementById('blur-background');
+const bodytag = document.getElementById('body');
+
+//TODO figure out how to hide an individual project instead of hiding all projects
+blurBackground.addEventListener('click', hideAllOpenedProjects)
+
+function hideAllOpenedProjects(){
+    const projectDetailBoxes = document.querySelectorAll('.project-details');
+    projectDetailBoxes.forEach((box, index) => {
+        blurBackground.style.visibility = 'hidden';
+        blurBackground.style.opacity = '0';
+        box.style.display = 'none'
+        bodytag.style.overflow = 'scroll';
+    });
+}
+
+function hideContent(projectID) {
+    const project = document.getElementById(projectID);
+    blurBackground.style.visibility = 'hidden';
+    blurBackground.style.opacity = '0';
+    project.style.display = 'none';
+    bodytag.style.overflow = 'scroll';
+}
+
+function showDetails(projectID){
+    const project = document.getElementById(projectID);
+    blurBackground.style.visibility = 'visible';
+    blurBackground.style.opacity = '1';
+    project.style.display = 'block';
+    bodytag.style.overflow = 'hidden';
+}
